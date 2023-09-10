2a206bcc6a59931c28457339667a2c31e15d6975
salarsalimi <33908999+salarsalimi@users.noreply.github.com>
2023-09-10 19:59:38 +0330

    Update helpers.c

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,25 +1,122 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            BYTE avg = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+            image[i][j].rgbtRed = avg;
+            image[i][j].rgbtGreen = avg;
+            image[i][j].rgbtBlue = avg;
+        }
+    }
     return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sepiaRed = round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue);
+            int sepiaGreen = round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue);
+            int sepiaBlue = round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue);
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+        }
+    }
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < (width / 2); j++)
+        {
+            BYTE TMPR = image[i][j].rgbtRed;
+            BYTE TMPG = image[i][j].rgbtGreen;
+            BYTE TMPB = image[i][j].rgbtBlue;
+
+            image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
+            image[i][j].rgbtGreen = image[i][width - j - 1].rgbtGreen;
+            image[i][j].rgbtBlue = image[i][width - j - 1].rgbtBlue;
+
+            image[i][width - j - 1].rgbtRed = TMPR;
+            image[i][width - j - 1].rgbtGreen = TMPG;
+            image[i][width - j - 1].rgbtBlue = TMPB;
+        }
+    }
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // tmp img
+    RGBTRIPLE temp[height][width];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+
+            int t_red, t_blue, t_green;
+            t_red = t_blue = t_green = 0;
+            float counter = 0.00;
+            for (int h = i - 1; h <= i + 1; h++)
+            {
+                if (h < 0 || h > height - 1)
+                {
+                    continue;
+                }
+                for (int w = j - 1; w <= j + 1; w++)
+                {
+                    if (w < 0 || w > width - 1)
+                    {
+                        continue;
+                    }
+                    counter++;
+                    t_red += image[h][w].rgbtRed;
+                    t_green += image[h][w].rgbtGreen;
+                    t_blue += image[h][w].rgbtBlue;
+                }
+            }
+            temp[i][j].rgbtRed = round(t_red / counter);
+            temp[i][j].rgbtGreen = round(t_green / counter);
+            temp[i][j].rgbtBlue = round(t_blue / counter);
+        }
+    }
+
+    // replace tmp witt original
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtRed = temp[i][j].rgbtRed;
+            image[i][j].rgbtGreen = temp[i][j].rgbtGreen;
+            image[i][j].rgbtBlue = temp[i][j].rgbtBlue;
+        }
+    }
     return;
 }
